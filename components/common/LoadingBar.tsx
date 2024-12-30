f080155789df6b6fe143aedd4bde045731572296
poya0a <damdadodam1@gmail.com>
2024-12-30 12:52:03 +0900

    .

--- components/common/LoadingBar.tsx
+++ components/common/LoadingBar.tsx
@@ -1,7 +1,7 @@
 import { useLoadingStore } from "@stores/useLoadingStore";
 import { BeatLoader } from "react-spinners";
 
-const Loading = () => {
+const LoadingBar = () => {
   const { isLoading } = useLoadingStore();
 
   return (
@@ -34,4 +34,4 @@ const Loading = () => {
   );
 };
 
-export default Loading;
+export default LoadingBar;
--- provider/providers.tsx
+++ provider/providers.tsx
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 import { onScrollLock, onScrollUnlock } from "@utils/scroll";
 import { useScrollLockStore } from "@stores/useScrollLockStore";
-import Loading from "@components/common/Loading";
+import LoadingBar from "@/components/common/LoadingBar";
 import storage from "@fetch/auth/storage";
 import { usePathname } from "next/navigation";
 
@@ -33,7 +33,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       {children}
-      <Loading />
+      <LoadingBar />
       <ScrollLockHandler />
     </>
   );
