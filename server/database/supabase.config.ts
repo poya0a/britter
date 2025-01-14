58c684fa73960409ef14b783a13a50b9a5d22c5b
doha <damdadodam1@gmail.com>
2025-01-14 10:45:10 +0900

    .

--- server/database/supabase.config.ts
+++ server/database/supabase.config.ts
@@ -6,12 +6,12 @@ import { createClient } from '@supabase/supabase-js'
 
 config();
 
-const { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } = process.env;
+const { NEXT_PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY } = process.env;
 
-if (!NEXT_PUBLIC_SUPABASE_URL || !NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+if (!NEXT_PUBLIC_SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
   throw new Error("서버 정보가 올바르지 않습니다.");
 }
 
-const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY)
+const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
 
 export default supabase;
