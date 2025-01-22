c5c635626d20af62ececbe61c9f69970b3dde9b6
doha <damdadodam1@gmail.com>
2025-01-22 10:25:56 +0900

    .

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -365,7 +365,7 @@ export default function Join() {
       const terms = useTermsState.filter((term) => term.checked).map((term) => term.seq);
 
       for (const key in data) {
-        formData.append(key, data[key]);
+        formData.append(key, JSON.stringify(data[key]));
       }
 
       formData.append("terms", JSON.stringify(terms));
@@ -378,7 +378,6 @@ export default function Join() {
       try {
         const res = await fetch("api/auth/join", {
           method: "POST",
-          headers: { "Content-Type": "multipart/form-data" },
           body: formData,
         });
 
